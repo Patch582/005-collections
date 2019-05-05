9fcd216e79dd294c8ac8b7290cf0089b2769e911
Pat Booth <patrickbooth582@gmail.com>
2019-05-05 13:17:33 +1000

    Created program.py

---
+++ program.py
@@ -0,0 +1,15 @@
+
+
+def print_header():
+    print('------------------------------------------')
+    print('             CSV Parser App')
+    print('------------------------------------------')
+    print()
+
+
+def main():
+    print_header()
+
+
+if __name__ == '__main__':
+    main()
